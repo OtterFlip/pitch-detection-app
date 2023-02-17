8652b25059b82005325307b051d2bbf854a93678
Nate Bushman <nbushman@gmail.com>
2023-02-17 15:57:43 -0700

    fixed minor typos

--- client/src/components/main/about.tsx
+++ client/src/components/main/about.tsx
@@ -34,7 +34,7 @@ export function About() {
       </span>
       <br />
       <h3>Usage</h3>
-      <span>Click on the play button to start training.  You will be repeatedly prompted to play a random note.  Upon detection, you'll be prompted to play a new random note.  For instruments, it's best to memorize in a cumulative manner. For instance with a guitar you could start on one string such as the highest string and memorize all the notes and then add the second string by playing notes from both the first and second strings and so on.  If you learn one string and then switch to learning a different string without continuing to practice the first string then you'll be more likey to confuse your brain and forget the notes on the first string.</span>
+      <span>Click on the play button to start training.  You will be repeatedly prompted to play a random note.  Upon detection, you'll be prompted to play a new random note.  For instruments, it's best to memorize in a cumulative manner. For instance with a guitar you could start on one string such as the highest string and memorize all the notes and then add the second string by playing notes from both the first and second strings and so on.  If you learn one string and then switch to learning a different string without continuing to practice the first string then you'll be more likely to confuse your brain and forget the notes on the first string.</span>
     </div>
   );
 }
